a3350b4fdb5de1bf17e8d0b105205ee765fc0ba0
Matthew Horan <112355773+mhorandata@users.noreply.github.com>
2023-07-27 16:22:02 -0600

    Create MyFirstApp.cpp

---
+++ MyFirstApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; //Instead of writing std::count << all the time you can make the std global by typing using namespace std; at the top below the #include <iostream>
+
+int main()
+{
+    //Before adding using namespace std; at the top:  std::cout << "Hello World!\n"; Now (notice that it starts with cout instead of std::cout <<
+   cout << "Hello World!\n"; //the \n" is a line Break. You can also do <<std::endl; to create a line break.
+   cout << "My name is Matthew" << std::endl; 
+
+
+    system("pause>0");
+}
+//Current video location: https://youtu.be/GQp1zzTwrIg?list=RDCMUCl5-BV9aRaeDVohpE4sqJiQ&t=1441
